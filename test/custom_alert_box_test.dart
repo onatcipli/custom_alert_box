884798252745735034d8a2fd12173587cd653eaf
onatcipli <onatcipli@outlook.com>
2019-06-15 18:46:03 +0300

    Example updated, CustomAlertBox Updated without test

--- test/custom_alert_box_test.dart
+++ test/custom_alert_box_test.dart
@@ -4,10 +4,13 @@ import 'package:custom_alert_box/custom_alert_box.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+//    final calculator = Calculator();
+//    expect(calculator.addOne(2), 3);
+//    expect(calculator.addOne(-7), -6);
+//    expect(calculator.addOne(0), 1);
+//    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+//    expect(() {
+//      return CustomAlertBox.showCustomAlertBox(context: null, willDisplayWidget: null);
+//    }, () => null);
   });
 }
